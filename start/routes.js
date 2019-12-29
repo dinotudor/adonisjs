814b5d283a0c8f85fba3bfae63cc7e1bbeb1d818
Dino Tudor <dinotudor@gmail.com>
2019-12-29 22:31:35 +0100

    Password Post Route

--- start/routes.js
+++ start/routes.js
@@ -4,3 +4,5 @@ const Route = use("Route");
 
 Route.post("users", "userController.store");
 Route.post("sessions", "SessionController.store");
+
+Route.post("passwords", "ForgotPasswordController.store");
