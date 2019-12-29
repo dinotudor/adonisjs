61a6127c5a83b311a3ca6b0bafbb092febf7092c
Dino Tudor <dinotudor@gmail.com>
2019-12-29 22:44:37 +0100

    try catch error for invalid email

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -4,14 +4,20 @@ const crypto = require("crypto");
 const User = use("App/Models/User");
 
 class ForgotPasswordController {
-  async store({ request }) {
-    const email = request.input("email");
-    const user = await User.findBy("email", email);
+  async store({ request, response }) {
+    try {
+      const email = request.input("email");
+      const user = await User.findByOrFail("email", email);
 
-    user.token = crypto.randomBytes(10).toString("hex");
-    user.token_created_at = new Date();
+      user.token = crypto.randomBytes(10).toString("hex");
+      user.token_created_at = new Date();
 
-    await user.save();
+      await user.save();
+    } catch (err) {
+      return response
+        .status(err.status)
+        .send({ error: { message: "Email inválido" } });
+    }
   }
 }
 
