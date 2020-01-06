e54dd8d26d3102fea4de5f959d3f9d20bcb7e2d4
Dino Tudor <dinotudor@gmail.com>
2020-01-06 22:57:49 +0100

    file controller store method

--- app/Controllers/Http/FileController.js
+++ app/Controllers/Http/FileController.js
@@ -4,7 +4,34 @@ const File = use("App/Models/File");
 const Helpers = use("Helpers");
 
 class FileController {
-  async store({ request, response }) {}
+  async store({ request, response }) {
+    try {
+      if (!request.file('file')) return
+
+      const upload = request.file('file', { size: '2mb' })
+
+      const fileName = `${Date.now()}.${upload.subtype}`
+
+      await upload.move(Helpers.tmpPath('uploads'), {
+        name: fileName
+      })
+
+      if (!upload.move()) {
+        throw upload.error()
+      }
+
+      const file = await File.create({
+        file: fileName,
+        name: upload.clientName,
+        type: upload.type
+        subtype: upload.subtype
+      })
+
+      return file
+    } catch (err) {
+      return response.status(err.status).send({  message: "Upload failed"})
+    }
+  }
 }
 
 module.exports = FileController;
